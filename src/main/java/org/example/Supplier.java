65270341edc767b06643ef5e9d6fe9a00b511a76
toukay <35756997+toukay@users.noreply.github.com>
2024-01-03 12:19:27 +0100

    Implement Supplier class.

--- src/main/java/org/example/Supplier.java
+++ src/main/java/org/example/Supplier.java
@@ -1,5 +1,40 @@
 package org.example;
 
-public class Supplier {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Supplier {
+    private static ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
+    private static int supplierIdCounter = 1;
+    private int supplierId;
+    private String name;
+    private String address;
+
+    public Supplier(String name, String address) {
+        this.supplierId = supplierIdCounter++;
+        suppliers.add(supplierId, this);
+
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public static List<Supplier> getAllSuppliers() {
+        return new ArrayList<>(suppliers);
+    }
 }
