dd700c6054a7591ab3f6caff1d01f2666c666d97
toukay <35756997+toukay@users.noreply.github.com>
2024-01-03 12:19:40 +0100

    Implement SupplyOrder class.

--- src/main/java/org/example/SupplyOrder.java
+++ src/main/java/org/example/SupplyOrder.java
@@ -1,5 +1,69 @@
 package org.example;
 
-public class SupplyOrder {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SupplyOrder {
+    private static int nextOrderId = 1;
+    private int orderId;
+    private Manager orderedBy;
+    private Medication medication;
+    private Supplier supplier;
+    private SupplyOrderStatus status;
+
+    // Constructor
+    private SupplyOrder(Manager orderedBy, Medication medication, Supplier supplier) {
+        this.orderId = nextOrderId++;
+        this.orderedBy = orderedBy;
+        this.medication = medication;
+        this.supplier = supplier;
+        this.status = SupplyOrderStatus.ORDERED;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public Manager getOrderedBy() {
+        return orderedBy;
+    }
+
+    public void setOrderedBy(Manager orderedBy) {
+        this.orderedBy = orderedBy;
+    }
+
+    public Medication getMedication() {
+        return medication;
+    }
+
+    public void setMedication(Medication medication) {
+        this.medication = medication;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public SupplyOrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SupplyOrderStatus status) {
+        this.status = status;
+    }
+
+    public static SupplyOrder orderMedicine(Manager manager, Medication medication, Supplier supplier) {
+        return new SupplyOrder(manager, medication, supplier);
+    }
+}
+
+enum SupplyOrderStatus {
+    ORDERED,
+    INTRANSIT,
+    DELIVERED,
+    CANCELLED
 }
