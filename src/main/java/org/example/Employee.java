2e04330203443719cb39b53a96e2d4d95770b05c
toukay <35756997+toukay@users.noreply.github.com>
2024-01-03 09:59:35 +0100

    Update Request.

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -5,4 +5,9 @@ public class Employee extends Person {
 	public Employee(String name, String surname, String phone, String pesel, int hourlyRate) {
 		super(name, surname, phone, pesel, hourlyRate);
 	}
+
+	@Override
+	public int calculateSalary() {
+		return super.calculateSalary();
+	}
 }
--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Request {
@@ -7,30 +8,39 @@ public class Request {
 	private static int requestIdCounter = 1;
 
 	private int requestId;
+	private RequestType requestType;
 	private String name;
 	private String description;
 	private Employee creator;
 	private Manager reviewer;
 	private boolean reviewed;
 	private boolean accepted;
+	private LocalDateTime creationDate;
+	private LocalDateTime reviewDate;
 
 	private static ArrayList<Request> requests = new ArrayList<Request>();
 
-	public Request(String name, String description, Employee creator) {
+	private Request(RequestType requestType, String name, String description, Employee creator) {
 		this.requestId = requestIdCounter++;
 		requests.add(requestId, this);
 
+		this.requestType = requestType;
 		this.name = name;
 		this.description = description;
 		this.creator = creator;
 		this.reviewer = null;
 		this.reviewed = false;
 		this.accepted = false;
+
+		this.creationDate = LocalDateTime.now();
+		this.reviewDate = null;
 	}
 
 	public int getRequestId() {
 		return requestId;
 	}
+
+	public RequestType getRequestType() {return requestType; }
 
 	public String getName() {
 		return name;
@@ -43,6 +53,8 @@ public class Request {
 	public Employee getCreator() {
 		return creator;
 	}
+
+	public LocalDateTime getCreationDate() { return creationDate; }
 	
 	public boolean isReviewed() {
 		return reviewed;
@@ -60,10 +72,25 @@ public class Request {
 		this.reviewer = reviewer;
 		this.reviewed = true;
 		this.accepted = verdict;
+		this.reviewDate = LocalDateTime.now();
 	}
+
+	public LocalDateTime getReviewDate() {return reviewDate; }
 
 	public static ArrayList<Request> viewRequests() {
 		return requests;
 	}
-	
+
+	public static Request requestNewMedicine(Employee creator, String name, String description) {
+		return new Request(RequestType.NEW_MED, name, description, creator);
+	}
+
+	public static Request requestResupply(Employee creator, String name, String description) {
+		return new Request(RequestType.RESUPPLY, name, description, creator);
+	}
+}
+
+enum RequestType {
+	NEW_MED,
+	RESUPPLY
 }
